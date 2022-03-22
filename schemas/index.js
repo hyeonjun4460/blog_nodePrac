97228bc3db129db54365905ac826d1dfd7516790
hyeonjun4460 <hyeonjun4460@gmail.com>
2022-03-22 23:58:46 +0900

    주석 추가 - module 보내는 것 관련 주석 추가.

--- schemas/index.js
+++ schemas/index.js
@@ -7,8 +7,10 @@ const express = require('express')
 // db 연결
 const connect = () => {
     mongoose.connect('mongodb://localhost:27017/blog_nodePrac', { ignoreUndefined: true }).catch((err) => {
-        console.error(err)
+        console.error(err) // 서버 입장에선 localhost임
     })
 }
 
+
+// connect란 이름으로 모듈 추가.
 module.exports = connect
