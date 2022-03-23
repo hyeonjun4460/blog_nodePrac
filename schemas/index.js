cc512e0b80c8f774d1fbf52c995793ac48d05433
hyeonjun4460 <hyeonjun4460@gmail.com>
2022-03-23 10:13:11 +0900

    mongoDB 연결

--- schemas/index.js
+++ schemas/index.js
@@ -1,15 +1,15 @@
 //  db 연결 스키마
 
 //  mongoDB 컨트롤 위한 모듈 생성
-const { default: mongoose } = require('mongoose')
+const mongoose = require('mongoose')
 const express = require('express')
 
-const mongoId = process.env.ID
-const mongopw = process.env.PW
 
 // db 연결
 const connect = () => {
-    mongoose.connect(`mongodb://${mongoId}:${mongopw}@localhost:27017/blog_nodePrac?authSource=admin&authMechanism=SCRAM-SHA-1`, { ignoreUndefined: true }).catch((err) => {
+    const mongoId = process.env.ID
+    const mongopw = process.env.PW
+    mongoose.connect(`mongodb://${mongoId}:${mongopw}@13.125.247.222:27017/blog_nodePrac?authSource=admin&authMechanism=SCRAM-SHA-1`, { ignoreUndefined: true }).catch((err) => {
         console.error(err) // 서버 입장에선 localhost임
     })
 }
